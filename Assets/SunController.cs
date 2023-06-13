af018c1dd0993236e2864b7b40fb71a50ed8941a
eglekat <egle.katilauskaite@gmail.com>
2023-06-13 17:13:48 +0100

    Make sun move

---
+++ Assets/SunController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunController : MonoBehaviour
+{
+
+    private Light light;
+    private float times;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = this.gameObject.transform.GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.times = Time.deltaTime * 1;
+        UpdateSunLocation(); 
+    }
+
+    void UpdateSunLocation() {
+        this.gameObject.transform.RotateAround(Vector3.up, Vector3.forward, times);
+    }
+
+    void ResetSunLocation() {
+        
+        this.gameObject.transform.position = new Vector3(0, 0, 0);
+    
+    }
+}

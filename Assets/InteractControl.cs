289dbe998534df8fba33cd61bab1fbd1326b149f
Louis Holdsworth <33750128+LouisH98@users.noreply.github.com>
2023-06-15 11:44:21 +0100

    refactor interact controls

--- Assets/InteractControl.cs
+++ Assets/InteractControl.cs
@@ -8,13 +8,6 @@ public class InteractControl : MonoBehaviour
     public double treeInteractionDistance = 2.0;
     Ray ray;
     RaycastHit hitData;
-    private GameObject currentlyOutlined;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
@@ -24,75 +17,21 @@ public class InteractControl : MonoBehaviour
         if (Physics.Raycast(ray, out hitData) && hitData.distance < treeInteractionDistance) {
             GameObject target = hitData.transform.gameObject;
 
-            // outlining TODO: buggy
-            if (target != currentlyOutlined) {
-                if (currentlyOutlined != null) {
-                    ToggleOutline(currentlyOutlined);
-                }
-                ToggleOutline(target);
-                currentlyOutlined = target;
-            }
-
             // e interaction
             if (Input.GetKeyDown("e")) {
-                Debug.Log("E pressed...");
-                Debug.Log($"Hit tag: {target.tag}");
-                if(target.tag == "tree") {
-                    Debug.Log($"Distance to tree: {hitData.distance}");
-
-                    Choppable treeChoppingScript = target.GetComponent<Choppable>();
-                    if (treeChoppingScript != null) {
-                        treeChoppingScript.Chop();
-                    }
-                  
-                }
-                else if(target.tag == "fire")
+                // Get the script that implements IInteractable, and call Interact()
+                var tempMonoArray = hitData.transform.gameObject.GetComponents<MonoBehaviour>();
+ 
+                foreach (var monoBehaviour in tempMonoArray)
                 {
-                    Debug.Log("Interact with fire");
-                    PlayerData playerData = gameObject.GetComponent<PlayerData>();
-
-                    if(playerData.GetWood() > 0) {
-                        FeedFire fireScript = target.GetComponent<FeedFire>();
-                        playerData.GetWood(-1);
-                        fireScript.AddWood(1);
+        
+                    if (monoBehaviour is IInteractable tempInteractable)
+                    {
+                        tempInteractable.Interact(hitData);
+                        break;
                     }
-                }
-                else if (target.tag == "log")
-                {
-                    Debug.Log("Interact with log");
-                    var fixedJoint = camera.GetComponent<FixedJoint>();
-                    var targetRigidBody = target.GetComponent<Rigidbody>();
-                    if (fixedJoint != null) {
-                        Destroy(fixedJoint);
-                    } else {
-                        // connect
-                        fixedJoint = camera.gameObject.AddComponent<FixedJoint>();
-                        fixedJoint.connectedBody = targetRigidBody;
-                        fixedJoint.connectedAnchor = target.transform.position;
-                        fixedJoint.enableCollision = false;
-                        fixedJoint.anchor = transform.position;
-                    }
-                } else if (target.tag == "ground") {
-                    // TODO: plant a tree 
-                }
+                }  
             }
-        } else if (currentlyOutlined != null) {
-            // we are now not looking at this object
-            ToggleOutline(currentlyOutlined);
-        }
-    }
-
-    private void ToggleOutline(GameObject g) {
-        return; // TODO: currently a bit buggy
-        if (g.tag != "log") { // TODO: use some better discriminator
-            return;
-        }
-
-        var outline = g.GetComponent<cakeslice.Outline>();
-        if (outline != null) {
-            Destroy(outline);
-        } else {
-            g.AddComponent<cakeslice.Outline>();
-        }
+        } 
     }
 }

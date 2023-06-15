2c96804f373265eeccaeca930fe7d396b525a94a
eglekat <egle.katilauskaite@gmail.com>
2023-06-15 18:05:30 +0100

    Light up the fire on start

--- Assets/StoryStart.cs
+++ Assets/StoryStart.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class StoryStart : MonoBehaviour
 {
     public AudioSource lightning;
@@ -10,12 +11,15 @@ public class StoryStart : MonoBehaviour
     private AudioSource startMusicSource;
     private GameObject player;
     private GameObject playerCamera;
+    private GameObject lightningLight;
     // Start is called before the first frame update
     void Start()
     {
         startMusicSource = startMusic.GetComponent<AudioSource>();
         player = GameObject.FindGameObjectWithTag("Player");
         playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        lightningLight = GameObject.FindGameObjectWithTag("lightning");
+
         //disable player
         player.GetComponent<FirstPersonMovement>().LockMovement();
         playerCamera.GetComponent<FirstPersonLook>().LockCamera();
@@ -26,10 +30,11 @@ public class StoryStart : MonoBehaviour
             w.color = new Color(w.color.r, w.color.g, w.color.b, 0);
         }
         StartCoroutine(RevealText());
-        
+
     }
-    IEnumerator RevealText() {
-        for (int word = 0; word < words.Length; word++) 
+    IEnumerator RevealText()
+    {
+        for (int word = 0; word < words.Length; word++)
         {
             StartCoroutine(FadeTextToFullAlpha(1f, words[word]));
             yield return new WaitForSeconds(2f);
@@ -39,16 +44,94 @@ public class StoryStart : MonoBehaviour
         // pause...
         yield return new WaitForSeconds(2f);
         GameObject.FindGameObjectWithTag("Sun").GetComponent<SunController>().SetTime(12);
-        lightning.Play();
+
         //disable black screen
-        gameObject.SetActive(false);
-        // yield return new WaitForSeconds(2f);
+        gameObject.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+
+        lightning.Play();
+
+        yield return FlashLightningLights();
+        // lightningLight.SetActive(false); todo remove this
+
+        yield return new WaitForSeconds(2f);
         //re-enable player
         player.GetComponent<FirstPersonMovement>().UnlockMovement();
         playerCamera.GetComponent<FirstPersonLook>().UnlockCamera();
+
+
+        //fade out
+        Light light = lightningLight.GetComponent<Light>();
+        float i = light.spotAngle;
+
+        while (lightningLight.transform.localPosition.y > -50)
+        {
+            lightningLight.transform.localPosition = new Vector3(lightningLight.transform.localPosition.x, lightningLight.transform.localPosition.y - 0.1f, lightningLight.transform.localPosition.z);
+            yield return new WaitForSeconds(.25f);
+        }
     }
 
-    void HideText( ) {
+
+    IEnumerator FlashLightningLights()
+    {
+
+        Debug.Log("lightning is playing");
+
+        Color red = new Color(255, 0, 0, 255);
+        Color yellow = new Color(255, 255, 0, 255);
+        Color white = new Color(255, 255, 255, 255);
+        Light light = lightningLight.GetComponent<Light>();
+
+        light.color = red;
+        light.intensity = 500;
+        yield return new WaitForSeconds(.2f);
+
+        light.color = yellow;
+        light.intensity = 100;
+
+        yield return new WaitForSeconds(.2f);
+
+        light.color = red;
+        light.intensity = 50;
+
+        yield return new WaitForSeconds(.2f);
+
+        light.color = yellow;
+        light.intensity = 100;
+
+        yield return new WaitForSeconds(.2f);
+
+        light.color = white;
+        light.intensity = 50;
+
+        yield return new WaitForSeconds(.2f);
+
+        light.color = yellow;
+        light.intensity = 100;
+
+        yield return new WaitForSeconds(.2f);
+
+        light.color = red;
+        light.intensity = 50;
+
+        yield return new WaitForSeconds(.2f);
+
+        light.color = yellow;
+        light.intensity = 100;
+
+        yield return new WaitForSeconds(.2f);
+
+        light.color = white;
+        light.intensity = 50;
+
+        yield return new WaitForSeconds(.2f);
+
+        light.intensity = 100;
+        light.color = white;
+
+
+    }
+    void HideText()
+    {
         foreach (TMP_Text w in words)
         {
             w.color = new Color(w.color.r, w.color.g, w.color.b, 0);

5c03f0de48474b8c27758e06a73c26ad9273c141
Louis Holdsworth <33750128+LouisH98@users.noreply.github.com>
2023-06-16 10:58:30 +0100

    rename ScaleUp to ScaleTransform

--- Assets/PlantSapling.cs
+++ Assets/PlantSapling.cs
@@ -10,25 +10,27 @@ public class PlantSapling : MonoBehaviour, IPickupable
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void OnPickup() {
+    public void OnPickup()
+    {
         isPickedUp = true;
     }
 
-   
+
     // when the plant collides with the floor, it should be planted
-     void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
-       // instanciate a new tree prefab where the sapling is, destroy the sapling
-         if (collision.gameObject.tag == "ground" && this.isPickedUp) {
+        // instanciate a new tree prefab where the sapling is, destroy the sapling
+        if (collision.gameObject.tag == "ground" && this.isPickedUp)
+        {
             // instanciate a new tree prefab where the sapling is, destroy the sapling
             GameObject newTree = Instantiate(treePrefab, transform.position, Quaternion.identity);
             var treeGrowth = newTree.GetComponent<TreeGrowth>();
@@ -36,8 +38,8 @@ public class PlantSapling : MonoBehaviour, IPickupable
             treeGrowth.StartTreeGrowing();
             // scale the tree up to its normal size over time
             var currentScale = gameObject.transform.localScale;
-            newTree.GetComponent<ScaleUp>().StartScaleUp(currentScale, new Vector3(1f, 1f, 1f), 0.5f);
+            newTree.GetComponent<ScaleTransform>().StartScaleUp(currentScale, new Vector3(1f, 1f, 1f), 0.5f);
             Destroy(gameObject);
-         }
+        }
     }
 }
--- Assets/ScaleTransform.cs
+++ Assets/ScaleTransform.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScaleUp : MonoBehaviour
+public class ScaleTransform : MonoBehaviour
 {
 
-    public void StartScaleUp(Vector3 startScale, Vector3 targetScale, float duration) {
+    public void StartScaleUp(Vector3 startScale, Vector3 targetScale, float duration)
+    {
         StartCoroutine(ScaleUpOverTime(startScale, targetScale, duration));
     }
 
-    IEnumerator ScaleUpOverTime(Vector3 startScale, Vector3 targetScale, float duration) {
+    IEnumerator ScaleUpOverTime(Vector3 startScale, Vector3 targetScale, float duration)
+    {
         float elapsedTime = 0.0f;
 
-        while (elapsedTime < duration) {
+        while (elapsedTime < duration)
+        {
             gameObject.transform.localScale = Vector3.Lerp(startScale, targetScale, elapsedTime / duration);
             elapsedTime += Time.deltaTime * Time.timeScale; // multiply by Time.timeScale
             yield return null;

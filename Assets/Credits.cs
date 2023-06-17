ba73dbdf5e368db6a9bc480da8d1c59f86bfaad7
eglekat <egle.katilauskaite@gmail.com>
2023-06-17 10:17:05 +0100

    Add Credits

---
+++ Assets/Credits.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Credits : MonoBehaviour
+{
+    private GameObject credits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        credits = GameObject.FindGameObjectWithTag("credits");
+        credits.SetActive(false);
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if(credits.activeSelf)
+            {
+                credits.SetActive(false);
+            }
+            else
+            {
+                credits.SetActive(true);
+            }
+        }
+    }
+
+}
